894577a1556dad0af5bb9bc40ddfa94465b7c5d1
kaan <kaandesu00@gmail.com>
2024-10-02 00:02:28 +0200

    feat: gltf loader with examples

--- src/threesnapPlugin/index.ts
+++ src/threesnapPlugin/index.ts
@@ -1,11 +1,14 @@
 import "./style.css";
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 interface Page {
   dark?: boolean;
   x: number;
   y: number;
   z: number;
+  rotation?: { x: number; y: number; z: number };
+  scale?: { x: number; y: number; z: number };
 }
 
 // TODO: ask for a mesh or for a model
@@ -13,6 +16,30 @@ interface Config {
   fixedZ?: number;
   pages: Page[];
 }
+
+const degToRad = (deg: number): number => {
+  return (deg * 3.14) / 180;
+};
+
+const percentageToRotation = (rotation: {
+  x: number;
+  y: number;
+  z: number;
+}) => {
+  return {
+    x: degToRad(rotation.x),
+    y: degToRad(rotation.y),
+    z: degToRad(rotation.z),
+  };
+};
+
+const percentageToScale = (scale: { x: number; y: number; z: number }) => {
+  return {
+    x: scale.x,
+    y: scale.y,
+    z: scale.z,
+  };
+};
 
 // Function to create page indicators
 const createThreesnap = (config: Config): void => {
@@ -55,10 +82,51 @@ const createThreesnap = (config: Config): void => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.getElementById("three-container")?.appendChild(renderer.domElement);
 
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(10, 10, 10); // Position the light
+  scene.add(directionalLight);
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft white light
+  scene.add(ambientLight);
+
+  const loader = new GLTFLoader();
+  let model: THREE.Object3D;
+
+  // Load the GLB model
+  loader.load(
+    "../../public/apollo_11_command_module_exterior.glb", // replace with the actual path to your .glb file
+    function (gltf) {
+      model = gltf.scene;
+      const {
+        x: scaleX,
+        y: scaleY,
+        z: scaleZ,
+      }: {
+        x: number;
+        y: number;
+        z: number;
+      } = config.pages[0].scale ?? { x: 1, y: 1, z: 1 };
+      model.scale.set(scaleX, scaleY, scaleZ);
+
+      const {
+        x: posX,
+        y: posY,
+        z: posZ,
+      }: {
+        x: number;
+        y: number;
+        z: number;
+      } = config.pages[0].scale ?? { x: 0, y: 0, z: 0 };
+      model.position.set(posX, posY, posZ);
+
+      scene.add(model);
+    },
+    undefined,
+    function (error) {
+      console.error("An error happened while loading the model:", error);
+    }
+  );
+
   scene.background = null;
 
   camera.position.z = config.fixedZ ?? 5;
@@ -101,23 +169,72 @@ const createThreesnap = (config: Config): void => {
     const startPos = percentageToPosition(percentagePositions[startIndex]);
     const endPos = percentageToPosition(percentagePositions[endIndex]);
 
+    const startRotation = percentageToRotation(
+      percentagePositions[startIndex].rotation || { x: 0, y: 0, z: 0 }
+    );
+    const endRotation = percentageToRotation(
+      percentagePositions[endIndex].rotation || { x: 0, y: 0, z: 0 }
+    );
+
     const scrollFraction = fractionalPageIndex - startIndex;
 
-    cube.position.x = THREE.MathUtils.lerp(
-      startPos.x,
-      endPos.x,
+    model.rotation.x = THREE.MathUtils.lerp(
+      startRotation.x,
+      endRotation.x,
       scrollFraction
     );
-    cube.position.y = THREE.MathUtils.lerp(
-      startPos.y,
-      endPos.y,
+    model.rotation.y = THREE.MathUtils.lerp(
+      startRotation.y,
+      endRotation.y,
       scrollFraction
     );
-    cube.position.z = THREE.MathUtils.lerp(
-      startPos.z,
-      endPos.z,
+    model.rotation.z = THREE.MathUtils.lerp(
+      startRotation.z,
+      endRotation.z,
       scrollFraction
     );
+
+    // Update scale
+    const startScale = percentageToScale(
+      percentagePositions[startIndex].scale || { x: 1, y: 1, z: 1 }
+    );
+    const endScale = percentageToScale(
+      percentagePositions[endIndex].scale || { x: 1, y: 1, z: 1 }
+    );
+
+    model.scale.x = THREE.MathUtils.lerp(
+      startScale.x,
+      endScale.x,
+      scrollFraction
+    );
+    model.scale.y = THREE.MathUtils.lerp(
+      startScale.y,
+      endScale.y,
+      scrollFraction
+    );
+    model.scale.z = THREE.MathUtils.lerp(
+      startScale.z,
+      endScale.z,
+      scrollFraction
+    );
+
+    if (model) {
+      model.position.x = THREE.MathUtils.lerp(
+        startPos.x,
+        endPos.x,
+        scrollFraction
+      );
+      model.position.y = THREE.MathUtils.lerp(
+        startPos.y,
+        endPos.y,
+        scrollFraction
+      );
+      model.position.z = THREE.MathUtils.lerp(
+        startPos.z,
+        endPos.z,
+        scrollFraction
+      );
+    }
 
     let closestPageIndex = currentPageIndex;
     let minDistance = Infinity;
